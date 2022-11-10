fc962c4a6f8933576015536b117db5b17c64713e
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-10 19:01:12 +0600

    footer added

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -4,19 +4,13 @@ import Footer from '../Shared/Footer/Footer';
 import Header from '../Shared/Header/Header';
 
 const Main = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
-
-            <Footer></Footer>
-
-
-
-
-
-        </div>
-    );
+  return (
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
