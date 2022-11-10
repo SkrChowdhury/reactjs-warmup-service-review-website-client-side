b3d95be99c19981df892e3888e84595340c9e114
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-10 18:13:52 +0600

    header updated

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,8 +1,5 @@
-import React, { useContext } from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useContext, useState } from 'react';
 
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../context/UserContext';
 
@@ -16,68 +13,221 @@ const Header = () => {
       .catch((error) => console.error(error));
   };
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div>
-      <Navbar collapseOnSelect bg="light" expand="lg">
-        <Container>
-          <Link to="/">
-            <Button className="fw-bolder text-warning fs-2" variant="">
-              <span className="text-dark">F</span>IT
-              <span className="text-dark">B</span>AZZ
-            </Button>
-          </Link>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse className="justify-content-end ">
-            <Link to="/">
-              <Button className="fw-bolder" variant="">
-                Home
-              </Button>
-            </Link>
-            <Link to="/blog">
-              <Button className="fw-bolder" variant="">
-                Blog
-              </Button>
-            </Link>
-
-            {user?.email && (
-              <Link to="/orders">
-                <Button className="fw-bolder" variant="">
-                  My Reviews
-                </Button>
-              </Link>
-            )}
-
-            {user?.email && (
-              <Link to="/addService">
-                <Button className="fw-bolder" variant="">
-                  Add Service
-                </Button>
-              </Link>
-            )}
-
-            {/* {user?.email && <Nav.Link  to="/manageAllOrders">Manage All Orders</Nav.Link>} */}
-
-            {/* {!user?.email && < Link to='/login'><Button variant="outline-success">login</Button></Link>}
-                        {user?.email && <span>{user?.displayName}</span>} */}
-            {user?.email && <span>Welcome,{user.email}</span>}
-            <Link to="/login">
-              <Button className="fw-bolder" variant="">
-                Login
-              </Button>
-            </Link>
-
-            {user?.email ? (
-              <Button onClick={handleSignout} className="fw-bolder" variant="">
-                Logout
-              </Button>
-            ) : (
-              <Link to="/login">
-                <Button className="btn btn-small fw-bolder">Login</Button>
-              </Link>
-            )}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div class="bg-gray-900">
+        <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+          <div class="relative flex grid items-center grid-cols-2 lg:grid-cols-3">
+            <ul class="flex items-center hidden space-x-8 lg:flex">
+              <li>
+                <a
+                  href="/"
+                  aria-label="Our product"
+                  title="Our product"
+                  class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+                >
+                  Product
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  aria-label="Our product"
+                  title="Our product"
+                  class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+                >
+                  Features
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  aria-label="Product pricing"
+                  title="Product pricing"
+                  class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+                >
+                  Pricing
+                </a>
+              </li>
+            </ul>
+            <a
+              href="/"
+              aria-label="Warm Up"
+              title="Warm Up"
+              class="inline-flex items-center lg:mx-auto"
+            >
+              <svg
+                class="w-8 text-teal-accent-400"
+                viewBox="0 0 24 24"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                stroke="currentColor"
+                fill="none"
+              >
+                <rect x="3" y="1" width="7" height="12" />
+                <rect x="3" y="17" width="7" height="6" />
+                <rect x="14" y="1" width="7" height="6" />
+                <rect x="14" y="11" width="7" height="12" />
+              </svg>
+              <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+                Warm Up
+              </span>
+            </a>
+            <ul class="flex items-center hidden ml-auto space-x-8 lg:flex">
+              <li>
+                <a
+                  href="/"
+                  aria-label="Sign in"
+                  title="Sign in"
+                  class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+                >
+                  Sign in
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                  aria-label="Sign up"
+                  title="Sign up"
+                >
+                  Sign up
+                </a>
+              </li>
+            </ul>
+            <div class="ml-auto lg:hidden">
+              <button
+                aria-label="Open Menu"
+                title="Open Menu"
+                class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+                onClick={() => setIsMenuOpen(true)}
+              >
+                <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                  />
+                  <path
+                    fill="currentColor"
+                    d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                  />
+                  <path
+                    fill="currentColor"
+                    d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                  />
+                </svg>
+              </button>
+              {isMenuOpen && (
+                <div class="absolute top-0 left-0 w-full">
+                  <div class="p-5 bg-white border rounded shadow-sm">
+                    <div class="flex items-center justify-between mb-4">
+                      <div>
+                        <a
+                          href="/"
+                          aria-label="Warm Up"
+                          title="Warp Up"
+                          class="inline-flex items-center"
+                        >
+                          <svg
+                            class="w-8 text-deep-purple-accent-400"
+                            viewBox="0 0 24 24"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeMiterlimit="10"
+                            stroke="currentColor"
+                            fill="none"
+                          >
+                            <rect x="3" y="1" width="7" height="12" />
+                            <rect x="3" y="17" width="7" height="6" />
+                            <rect x="14" y="1" width="7" height="6" />
+                            <rect x="14" y="11" width="7" height="12" />
+                          </svg>
+                          <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                            Warm Up
+                          </span>
+                        </a>
+                      </div>
+                      <div>
+                        <button
+                          aria-label="Close Menu"
+                          title="Close Menu"
+                          class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                          onClick={() => setIsMenuOpen(false)}
+                        >
+                          <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                            <path
+                              fill="currentColor"
+                              d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <nav>
+                      <ul class="space-y-4">
+                        <li>
+                          <a
+                            href="/"
+                            aria-label="Our product"
+                            title="Our product"
+                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          >
+                            Product
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="/"
+                            aria-label="Our product"
+                            title="Our product"
+                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          >
+                            Features
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="/"
+                            aria-label="Product pricing"
+                            title="Product pricing"
+                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          >
+                            Pricing
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="/"
+                            aria-label="Sign in"
+                            title="Sign in"
+                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                          >
+                            Sign in
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="/"
+                            class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                            aria-label="Sign up"
+                            title="Sign up"
+                          >
+                            Sign up
+                          </a>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,113 +1,68 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { createContext } from 'react';
-import {createUserWithEmailAndPassword,getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
-import app from '../firebase/firebase.config'
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+import app from '../firebase/firebase.config';
 
-export const AuthContext=createContext()
-const Auth=getAuth(app)
+export const AuthContext = createContext();
+const Auth = getAuth(app);
 
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState({});
 
-const UserContext = ({children}) => {
+  const [loading, setLoading] = useState(true);
 
+  const googleProvider = new GoogleAuthProvider();
 
-const [user,setUser]=useState({})
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(Auth, email, password);
+  };
 
-const [loading,setLoading]=useState(true)
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(Auth, email, password);
+  };
 
-const googleProvider=new GoogleAuthProvider()
+  const signInWithGoogle = () => {
+    return signInWithPopup(Auth, googleProvider);
+  };
 
+  const logOut = () => {
+    return signOut(Auth);
+  };
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(Auth, (currentUser) => {
+      setUser(currentUser);
 
+      setLoading(false);
+    });
 
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-const createUser=(email,password)=>{
+  //const user={displayName:'Arkan'}
 
-
-return createUserWithEmailAndPassword(Auth,email,password)
-
-
-}
-
-
-const signIn=(email,password)=>{
-
-
-    return signInWithEmailAndPassword(Auth,email,password)
-    
-    
-    }
-
-
-    const signInWithGoogle=()=>{
-
-
-        return signInWithPopup(Auth,googleProvider)
-        
-        }
-
-
-
-
-
-
-
-
-    const logOut=()=>{
-
-        return signOut(Auth)
-        
-        
-        }
-
-
-    useEffect(()=>{
-
-        const unsubscribe=onAuthStateChanged(Auth,currentUser=>{
-        
-        setUser(currentUser)
-        
-        setLoading(false)
-        
-        
-        })
-        
-        return()=>{
-        
-        
-            unsubscribe()
-        }
-        
-        
-        } ,[])
-
-
-
-
-
-
-
-//const user={displayName:'Arkan'}
-    
-    
-    const authInfo={user,createUser,signIn,logOut,signInWithGoogle,loading}
-    return (
-
-        <AuthContext.Provider value={authInfo}>
-
-
-{children}
-
-
-        </AuthContext.Provider>
-            
-
-
-
-
-
-     
-    );
+  const authInfo = {
+    user,
+    createUser,
+    signIn,
+    logOut,
+    signInWithGoogle,
+    loading,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default UserContext;
